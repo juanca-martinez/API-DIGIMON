e39396d766cc92faa19af9c4b44155a51a9b2f8d
Juan ka <jcm.matus@gmail.com>
2023-04-19 21:35:57 -0400

    ver uso de card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,28 @@ var contenido = document.querySelector("#contenido");
 fetch(url)
   .then((response) => response.json())
   .then((datos) => {
-
     for (item of datos) {
-
       contenido.innerHTML += `
-            <tr>
-                <th >${item.name}</th>
-                <td><img src=${item.img} alt=${item.name} /></td>
-                <td>${item.level}</td>
-                
-            </tr>
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src=${item.img} alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.level}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>      
+      
         `;
+
       if (item.id == 10) {
         break;
       }
     }
   });
 
+/* <tr>
+                <th >${item.name}</th>
+                <td><img src=${item.img} alt=${item.name} /></td>
+                <td>${item.level}</td>
+                
+            </tr> */
