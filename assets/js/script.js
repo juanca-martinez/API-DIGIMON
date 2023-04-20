17b51a56889ccb69feb5604978a536ffbd717f7f
Juan ka <jcm.matus@gmail.com>
2023-04-19 21:01:58 -0400

    acomodar apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,24 @@
-var url = 'https://digimon-api.vercel.app/api/digimon'
+var url = "https://digimon-api.vercel.app/api/digimon";
 
-var contenido = document.querySelector("#contenido")
+var contenido = document.querySelector("#contenido");
 
 fetch(url)
-.then(response => response.json())
-.then(datos => {
-    //console.log(datos);
+  .then((response) => response.json())
+  .then((datos) => {
+
     for (item of datos) {
-    //for (var i =0; i<datos.length; i++)     item[i].userId
-        contenido.innerHTML +=  `
+
+      contenido.innerHTML += `
             <tr>
                 <th >${item.name}</th>
-                <td>${item.img}</td>
+                <td><img src=${item.img} alt=${item.name} /></td>
                 <td>${item.level}</td>
                 
             </tr>
-        `
-        if (item.id == 10) {break;}
+        `;
+      if (item.id == 10) {
+        break;
+      }
     }
-})  
+  });
 
-
-
-
-
-
-
-
-/*
-            <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-
-*/
