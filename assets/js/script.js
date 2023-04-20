8dad0618970e1f38a01104370fb0bb69c9a1ba48
Juan ka <jcm.matus@gmail.com>
2023-04-19 22:57:04 -0400

    faltan los eventos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-var url = "https://digimon-api.vercel.app/api/digimon";
+var url = "https://digimon-api.vercel.app/api/digimon?";
 
 var contenido = document.querySelector("#contenido");
+var numero = 0
 
 fetch(url)
   .then((response) => response.json())
   .then((datos) => {
     for (item of datos) {
+      numero++
       contenido.innerHTML += `
-      <div class="col-md-3" id="carta">
-        <div class="card" style="width: 18rem;">
+      <div class="col-md-4" id="carta">
+        <div class="card bg-info" style="width: 18rem;">
             <img class="card-img-top" src=${item.img} alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
-                <p class="card-text">${item.level}</p>
+                <h4 class="card-title text-white">${"#"+numero+" "}${item.name}</h4>
+                <p class="card-text text-light bg-dark text-center">${item.level}</p>
 
             </div>
         </div>
