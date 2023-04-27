31e14d2495a4bd2a1f981008dfe009daffc91e60
Juan ka <jcm.matus@gmail.com>
2023-04-26 22:30:56 -0400

    26-04-2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,36 @@ fetch(url)
     }
   });
 
+  function entrenar() {
+    var url = "https://digimon-api.vercel.app/api/digimon/level/In Training";
+var contenido = document.querySelector("#contenido");
+var numero = 0
+
+fetch(url)
+  .then((response) => response.json())
+  .then((datos) => {
+    for (item of datos) {
+      numero++
+      contenido.innerHTML += `
+      <div class="col-md-4" id="carta">
+        <div class="card">
+            <img class="card-img-top" src=${item.img} alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title text-white ver">${"#"+numero+" "}${item.name}</h4>
+                <p class="card-text text-light bg-dark text-center">${item.level}</p>
+
+            </div>
+        </div>
+    </div>
+        `;
+
+      if (numero == 100) {
+        break;
+      }
+    }
+  });
+
+}
   
 
 /* <tr>
